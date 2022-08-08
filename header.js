27670c10efca6963610f6c4ea5ed367cbc36791b
Edvin Berling <110623180+edvinberling@users.noreply.github.com>
2022-08-08 20:25:51 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,19 @@
+function openHome()
+{
+    window.location.href = "https://edvinberling.com";
+}
+
+function openAbout()
+{
+    window.location.href = "https://edvinberling.com/about";
+}
+
+function openProjects()
+{
+    window.location.href = "https://edvinberling.com/projects";
+}
+
+function openPlayground()
+{
+    window.location.href = "https://edvinberling.com/playground";
+}
